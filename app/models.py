c4f419080bf36e4db57fb942f2363f2212ab6525
Mary <mary.deroost@gmail.com>
2021-03-22 16:06:33 +0100

    Added new columns

--- app/models.py
+++ app/models.py
@@ -7,7 +7,9 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    nb_char = db.Column(db.Integer, default=0)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    edit_time = db.Column(db.DateTime, index=True)
     title = db.Column(db.Text)
 
     def __repr__(self):
