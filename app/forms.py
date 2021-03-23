adbe4ff5a1dfb428703d27fbc9ecfefefcaa038c
Mary <mary.deroost@gmail.com>
2021-03-23 13:12:39 +0100

    Added new functionalities

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,45 @@
 from flask_pagedown.fields import PageDownField
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired, Optional
 
 
-class CreatePost(FlaskForm):
-
-    title = StringField("Title", validators=[DataRequired()])
-    body = PageDownField("Body")
-    submit = SubmitField("Submit")
-
-
-class EditPost(FlaskForm):
+class AddOrEditPost(FlaskForm):
 
     title = StringField("Title", validators=[DataRequired()])
     body = PageDownField("Body")
-    submit = SubmitField("Submit edited post")
+
+    submit = SubmitField("to infinity and beyond!")
 
 
 class DeletePost(FlaskForm):
 
     submit = SubmitField("to the trash!")
+
+
+class AddOrEditLink(FlaskForm):
+
+    category = SelectField("Choose category")
+    add_category = StringField("Category")
+    url = StringField("URL")
+    description = TextAreaField("Description")
+    artist = StringField("Artist")
+    author = StringField("Author")
+    title = StringField("Title", validators=[DataRequired()])
+    year = IntegerField("Year", validators=[Optional()])
+    website = StringField("Website")
+    isbn = IntegerField("ISBN", validators=[Optional()])
+
+    submit = SubmitField("put that thing back where it came from or so help me")
+
+
+class DeleteLink(FlaskForm):
+
+    submit = SubmitField("be gone with it!")
+
+
+class PickCategory(FlaskForm):
+
+    category = SelectField("Choose category")
+
+    submit = SubmitField("ay, show me the results")
--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,9 @@ from sqlalchemy import desc
 from werkzeug.utils import redirect
 
 from app import app, db
-from app.forms import CreatePost, EditPost, DeletePost
-from app.models import Post
+from app.forms import DeletePost, PickCategory, DeleteLink, AddOrEditPost, AddOrEditLink
+from app.models import Post, Link
+from app.modules import pick_category, get_unique_categories
 
 
 @app.route("/")
@@ -18,12 +19,13 @@ def index():
     return render_template("index.html", title="Home", posts=posts)
 
 
-@app.route("/add", methods=['GET', 'POST'])
-def add():
+@app.route("/post_add", methods=['GET', 'POST'])
+def post_add():
 
-    form = CreatePost()
+    form = AddOrEditPost()
 
     if form.validate_on_submit():
+
         post = Post(body=form.body.data, title=form.title.data, nb_char=len(form.body.data))
         db.session.add(post)
         db.session.commit()
@@ -31,13 +33,13 @@ def add():
 
         return redirect(url_for('index'))
 
-    return render_template("add.html", title='Add Post', form=form)
+    return render_template("post_add.html", title='Add Post', form=form)
 
 
-@app.route("/edit/<string:id>", methods=['GET', 'POST'])
-def edit(id):
+@app.route("/post_edit/<string:id>", methods=['GET', 'POST'])
+def post_edit(id):
 
-    form = EditPost()
+    form = AddOrEditPost()
     post = Post.query.filter_by(id=int(id)).first()
 
     if request.method == "POST" and form.validate_on_submit():
@@ -54,11 +56,11 @@ def edit(id):
         form.title.data = post.title
         form.body.data = post.body
 
-    return render_template("edit.html", title="Edit post", form=form)
+    return render_template("post_edit.html", title="Edit post", form=form)
 
 
-@app.route("/delete/<string:id>", methods=['GET', 'POST'])
-def delete(id):
+@app.route("/post_delete/<string:id>", methods=['GET', 'POST'])
+def post_delete(id):
 
     form = DeletePost()
     post = Post.query.filter_by(id=int(id)).first()
@@ -70,7 +72,7 @@ def delete(id):
 
         return redirect(url_for("index"))
 
-    return render_template("delete.html", title="Confirm delete post", form=form, post=post)
+    return render_template("post_delete.html", title="Confirm delete post", form=form, post=post)
 
 
 @app.route("/post/<string:id>", methods=['GET'])
@@ -81,7 +83,123 @@ def post(id):
     return render_template("post.html", title=post.title, post=post)
 
 
-@app.route('/links')
+@app.route('/links', methods=['GET', 'POST'])
 def links():
 
-    return render_template("links.html")
+    pick = PickCategory()
+    pick.category.choices = get_unique_categories(all=True)
+
+    results = pick_category("All")
+
+    if pick.validate_on_submit():
+
+        chosen_pick = pick.category.data
+        if chosen_pick == "All":
+            results = pick_category("All")
+        else:
+            results = pick_category(chosen_pick)
+
+        if results.first() is None:
+            flash("nothing to see here, move on!")
+
+    return render_template("links.html", title="Links", pick=pick, results=results)
+
+
+@app.route('/link_add', methods=['GET', 'POST'])
+def link_add():
+
+    form = AddOrEditLink()
+    form.category.choices = get_unique_categories()
+
+    if form.validate_on_submit():
+
+        link = Link()
+
+        link.category = form.category.data
+        if link.category == "" and form.add_category.data:
+            link.category = form.add_category.data
+
+        link.url = form.url.data
+        link.description = form.description.data
+        link.artist = form.artist.data
+        link.author = form.author.data
+        link.title = form.title.data
+        link.year = form.year.data
+        link.website = form.website.data
+        link.isbn = form.isbn.data
+
+        db.session.add(link)
+        db.session.commit()
+
+        flash(f"{form.title.data} added successfully")
+
+        return redirect(url_for("links"))
+
+    return render_template('link_add.html', title="Add link", form=form)
+
+
+@app.route('/link_edit/<string:id>', methods=['GET', 'POST'])
+def link_edit(id):
+
+    form = AddOrEditLink()
+    form.category.choices = get_unique_categories()
+    link = Link.query.filter_by(id=int(id)).first()
+
+    if request.method == "POST" and form.validate_on_submit():
+
+        link.category = form.category.data
+        if link.category == "" and form.add_category.data:
+            link.category = form.add_category.data
+        link.url = form.url.data
+        link.description = form.description.data
+        link.artist = form.artist.data
+        link.author = form.author.data
+        link.title = form.title.data
+        link.year = form.year.data
+        link.website = form.website.data
+        link.isbn = form.isbn.data
+
+        db.session.commit()
+        flash(f"your changes on {link.title} have been saved.")
+
+        return redirect(url_for("links"))
+
+    elif request.method == "GET":
+
+        form.category.data = link.category
+        form.add_category.data = link.category
+        form.url.data = link.url
+        form.description.data = link.description
+        form.artist.data = link.artist
+        form.author.data = link.author
+        form.title.data = link.title
+        form.year.data = link.year
+        form.website.data = link.website
+        form.isbn.data = link.isbn
+
+    return render_template("link_edit.html", title="Edit link", form=form)
+
+
+@app.route("/link_delete/<string:id>", methods=['GET', 'POST'])
+def link_delete(id):
+
+    form = DeleteLink()
+    link = Link.query.filter_by(id=int(id)).first()
+
+    if form.validate_on_submit():
+
+        title = link.title
+
+        db.session.delete(link)
+        db.session.commit()
+        flash(f"the amigo {title} was successfully deleted")
+
+        return redirect(url_for("links"))
+
+    return render_template("link_delete.html", title="Confirm delete link", form=form, link=link)
+
+
+@app.route('/markdown_guide')
+def markdown_guide():
+
+    return render_template("markdown_guide.html", title="Markdown guide")
