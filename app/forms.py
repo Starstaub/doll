20052f98856cbcf57062c6470ff263f38d0dcc8d
Mary <mary.deroost@gmail.com>
2021-03-25 12:02:43 +0100

    Fixed mistakes + only one form for deletion

--- app/forms.py
+++ app/forms.py
@@ -8,55 +8,46 @@ from utils import TODO_STATUS
 
 class AddOrEditPost(FlaskForm):
 
-    title = StringField("Title", validators=[DataRequired(Length(min=0, max=100))])
+    title = StringField("Title", validators=[Length(min=0, max=100), DataRequired()])
     body = PageDownField("Body")
 
-    submit = SubmitField("to infinity and beyond!")
-
-
-class DeletePost(FlaskForm):
-
-    submit = SubmitField("to the trash!")
+    submit = SubmitField("Send")
 
 
 class AddOrEditLink(FlaskForm):
 
-    category = SelectField("Choose category")
-    add_category = StringField("Category", Length(min=0, max=50))
-    url = StringField("URL", Length(min=0, max=255))
+    category = SelectField("Category")
+    add_category = StringField("Category", validators=[Length(min=0, max=50)])
+    url = StringField("URL", validators=[Length(min=0, max=255)])
     description = TextAreaField("Description")
-    artist = StringField("Artist", Length(min=0, max=50))
-    author = StringField("Author", Length(min=0, max=50))
-    title = StringField("Title", validators=[DataRequired(Length(min=0, max=100))])
+    artist = StringField("Artist", validators=[Length(min=0, max=50)])
+    author = StringField("Author", validators=[Length(min=0, max=50)])
+    title = StringField("Title", validators=[Length(min=0, max=100), DataRequired()])
     year = IntegerField("Year", validators=[Optional()])
-    website = StringField("Website", Length(min=0, max=100))
+    website = StringField("Website", validators=[Length(min=0, max=100)])
     isbn = IntegerField("ISBN", validators=[Optional()])
 
-    submit = SubmitField("put that thing back where it came from or so help me")
-
-
-class DeleteLink(FlaskForm):
-
-    submit = SubmitField("be gone with it!")
+    submit = SubmitField("Send")
 
 
 class PickCategory(FlaskForm):
 
-    category = SelectField("Choose category")
+    category = SelectField("Category")
 
-    submit = SubmitField("ay, show me the results")
+    submit = SubmitField("Send")
 
 
 class AddOrEditTask(FlaskForm):
 
-    status = SelectField("Status", choices=TODO_STATUS)
-    title = StringField("Title", Length(min=0, max=100))
-    category = SelectField("Category")
-    description = TextAreaField("Description")
+    status = SelectField("Status", choices=TODO_STATUS, validators=[DataRequired()])
+    title = StringField("Title", validators=[Length(min=0, max=100), DataRequired()])
+    category = SelectField("Category", validators=[DataRequired()])
+    add_category = StringField("Category", validators=[Length(min=0, max=50), Optional()])
+    description = PageDownField("Description")
 
-    submit = SubmitField("add task")
+    submit = SubmitField("Send")
 
 
-class DeleteTask(FlaskForm):
+class DeleteItem(FlaskForm):
 
-    submit = SubmitField("bye bye")
+    submit = SubmitField("Delete permanently")
