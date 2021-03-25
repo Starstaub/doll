b54d47750a3fb2fc001b62ac382b2038919f2536
Mary <mary.deroost@gmail.com>
2021-03-25 12:04:33 +0100

    Removed "", now all uncategorized tasks = uncategorized

--- app/modules.py
+++ app/modules.py
@@ -1,26 +1,35 @@
 from app import db
-from app.models import Link
+from app.models import Link, Task
 
 
-def pick_category(category):
+def pick_category(model, category):
 
-    if category is "All":
-        return Link.query
-    if category == "Uncategorized":
-        category = ""
+    if category == "All":
+        if model == "link":
+            return Link.query
+        elif model == "task":
+            return Task.query
 
-    return Link.query.filter_by(category=category).order_by(Link.timestamp)
+    if model == "link":
+        return Link.query.filter_by(category=category).order_by(Link.timestamp)
+    elif model == "task":
+        return Task.query.filter_by(category=category).order_by(Task.created)
 
 
-def get_unique_categories(all=False):
+def get_unique_categories(model_name, all=False):
 
-    category_list = db.session.query(Link.category).distinct().all()
+    category_list = []
+
+    if model_name == 'link':
+        category_list = db.session.query(Link.category).distinct().all()
+    elif model_name == 'task':
+        category_list = db.session.query(Task.category).distinct().all()
+
     new_list = [i[0] for i in category_list]
 
     if all:
         new_list.insert(0, "All")
-        new_list = ["Uncategorized" if x == "" else x for x in new_list]
-    elif "" not in new_list:
-        new_list.insert(0, "")
+    elif "Uncategorized" not in new_list:
+        new_list.insert(0, "Uncategorized")
 
     return tuple((cat, cat) for cat in new_list)
