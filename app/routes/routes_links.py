3fb302e8edc9009b2c193e9551297b9cadd7ea79
Mary <mary.deroost@gmail.com>
2021-03-29 08:54:06 +0200

    Reducing code length

--- app/routes/routes_links.py
+++ app/routes/routes_links.py
@@ -29,15 +29,23 @@ def links():
     return render_template("links.html", title="Links", pick=pick, results=results)
 
 
-@app.route("/link_add", methods=["GET", "POST"])
-def link_add():
+@app.route("/link/add", methods=["GET", "POST"])
+@app.route("/link/edit/<string:id>", methods=["GET", "POST"])
+def link_add_edit(id=None):
 
     form = AddOrEditLink()
     form.category.choices = get_unique_categories("link")
 
-    if form.validate_on_submit():
-
+    if id:
+        link = Link.query.filter_by(id=int(id)).first()
+        title = "Edit link"
+        edit = True
+    else:
         link = Link()
+        title = "Add link"
+        edit = False
+
+    if request.method == 'POST' and form.validate_on_submit():
 
         link.category = form.category.data
         if link.category == "" and form.add_category.data:
@@ -55,46 +63,19 @@ def link_add():
         link.website = form.website.data
         link.isbn = form.isbn.data
 
-        db.session.add(link)
-        db.session.commit()
-
-        flash(f"The link '{form.title.data}' was successfully added.")
-
-        return redirect(url_for("links"))
-
-    return render_template("link_add.html", title="Add link", form=form)
-
-
-@app.route("/link_edit/<string:id>", methods=["GET", "POST"])
-def link_edit(id):
-
-    form = AddOrEditLink()
-    form.category.choices = get_unique_categories("link")
-    link = Link.query.filter_by(id=int(id)).first()
-
-    if request.method == "POST" and form.validate_on_submit():
-
-        link.category = form.category.data
-        if (link.category or link.category == "") and form.add_category.data:
-            link.category = form.add_category.data
-        link.url = form.url.data
-        link.description = form.description.data
-        link.artist = form.artist.data
-        link.author = form.author.data
-        link.title = form.title.data
-        link.year = form.year.data
-        link.website = form.website.data
-        link.isbn = form.isbn.data
-
-        db.session.commit()
-        flash(f"Your changes on '{link.title}' have been saved.")
+        if id:
+            db.session.commit()
+            flash(f"Your changes on '{link.title}' have been saved.")
+        else:
+            db.session.add(link)
+            db.session.commit()
+            flash(f"The link '{form.title.data}' was successfully added.")
 
         return redirect(url_for("links"))
 
-    elif request.method == "GET":
+    elif id and request.method == "GET":
 
         form.category.data = link.category
-        form.add_category.data = link.category
         form.url.data = link.url
         form.description.data = link.description
         form.artist.data = link.artist
@@ -104,10 +85,10 @@ def link_edit(id):
         form.website.data = link.website
         form.isbn.data = link.isbn
 
-    return render_template("link_edit.html", title="Edit link", form=form)
+    return render_template("link.html", title=title, form=form, edit=edit)
 
 
-@app.route("/link_delete/<string:id>", methods=["GET", "POST"])
+@app.route("/link/delete/<string:id>", methods=["GET", "POST"])
 def link_delete(id):
 
     form = DeleteItem()
